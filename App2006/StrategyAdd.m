53053808155815c32511dc63f2cd24116494d939
iOS-School-1 <iOS-School-1@iMac-user-8.local>
2017-06-20 21:14:21 +0300

    strategy

---
+++ App2006/StrategyAdd.m
@@ -0,0 +1,13 @@
+//
+//  StrategyAdd.m
+//  App2006
+//
+//  Created by iOS-School-1 on 20.06.17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import "StrategyAdd.h"
+
+@implementation StrategyAdd
+
+@end
