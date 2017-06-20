5832c432ab4b4848e68ef7cd9d416fa1655bf67f
user <iOS-School-1@iMac-user-8.local>
2017-06-20 19:36:22 +0300

    Initial Commit

---
+++ App2006/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  App2006
+//
+//  Created by iOS-School-1 on 20.06.17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
